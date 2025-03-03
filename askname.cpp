eec6a886501cd165c139c6843ba90536d720a3af
231006047 <filin.olga2007@gmail.com>
2025-03-03 09:06:23 +0700

    Update askname.cpp
    Ольга злодей!!!!!!!!!!!!!

--- askname.cpp
+++ askname.cpp
@@ -3,7 +3,8 @@
 
 void askname(std::string &first, std::string &last) {
     std::cout << "Enter your first name: ";
-    std::getline(std::cin, first); 
+    std::getline(std::cin, first); //мы изменили код >:)
+
 
     std::cout << "Now enter your last name: ";
     std::getline(std::cin, last); 
